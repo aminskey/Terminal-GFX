abfff0e9e675d90489021f12adca3f4de8accf7f
Amin <rhshaaf@gmail.com>
2025-12-03 12:41:27 +0100

    made some changes here

--- src/terminalgfx.c
+++ src/terminalgfx.c
@@ -1,14 +1,14 @@
 #include "terminalgfx.h"
 
 void tgfx_init(){
-    tgfx_settings_init();
-    tgfx_input_init();
-    tgfx_save_pos();
+  tgfx_settings_init();
+  tgfx_input_init();
+  tgfx_save_pos();
 }
 void tgfx_terminate(){
-    tgfx_closeInput();
-    tgfx_input_terminate();
-    tgfx_fb_quit();
-    tgfx_nocbreak();
-    tgfx_mv_savedpos();
+  tgfx_closeInput();
+  tgfx_input_terminate();
+  tgfx_fb_quit();
+  tgfx_nocbreak();
+  tgfx_mv_savedpos();
 }
